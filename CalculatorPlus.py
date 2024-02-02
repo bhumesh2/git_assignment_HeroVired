b1e6e1331fe3414342686befbbc8e89ec7d0f950
bhumesh2 <bhumesh.masram2@gmail.com>
2024-02-02 11:03:36 +0530

    commit in dev

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -2,22 +2,22 @@ import math
 
 class Calculator:
 
-def add(self, a, b):
-    return a + b
+    def add(self, a, b):
+        return a + b
 
-def subtract(self, a, b):
-    return a - b
+    def subtract(self, a, b):
+        return a - b
 
-def multiply(self, a, b):
+    def multiply(self, a, b):
 
-    return a * b
+        return a * b
 
-def divide(self, a, b):
-    if b == 0:
+    def divide(self, a, b):
+        if b == 0:
 
-        raise ValueError("Cannot divide by zero.")
+            raise ValueError("Cannot divide by zero.")
 
-    return a / b
+        return a / b
 # TODO: Implement the following function to calculate the square root of a number.
 
 # def square_root(self, x):
