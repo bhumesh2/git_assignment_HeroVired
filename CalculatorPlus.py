c365dbabe113db72e02c00991a389f9435e10024
bhumesh2 <bhumesh.masram2@gmail.com>
2024-02-03 16:51:58 +0530

    commit in feature/circle-area

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -19,6 +19,14 @@ class Calculator:
             raise ValueError("Cannot divide by zero.")
 
         return a / b
+    def calculate_rectangle_area(self, length, width):
+
+        return length * width
+    def calculate_circle_area(self, radius):
+
+        return math.pi * radius ** 2
+
+
 # TODO: Implement the following function to calculate the square root of a number.
 
     def square_root(self, x):
@@ -47,3 +55,14 @@ if __name__ == "__main__":
     num3 = 25
 
     print(f"The square root of {num3} = {calculator.square_root(num3)}")
+# TODO: Implement the feature to calculate the area of a rectangle # 
+    length = 10
+
+    width = 6
+
+    print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
+# TODO: Implement the feature to calculate the area of a circle
+
+    radius = 5
+
+    print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area(radius)}")
